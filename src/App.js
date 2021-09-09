a009c9f683f12487246ed70a83147039edb0dd69
Robby Noyes <robertjnoyes1@gmail.com>
2021-09-09 12:35:57 -0700

    build ready task app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask';
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -28,7 +29,9 @@ function App() {
 
   // Add Task
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
 
   //Delete Task
@@ -43,8 +46,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
